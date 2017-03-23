c303dbfe8923d1fd5bea271bac703d1731455f2d
Елена Баглаева < >
2017-03-23 23:31:30 +0700

    Task 5

---
+++ Лабораторная 5/lab5_2/lab3_12/lab3_12/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3_12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Графический редактор 1.1.", "О программе");
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (RadioButton_Ink.IsChecked == true) 
+               inkcanvas1.EditingMode = InkCanvasEditingMode.Ink;
+            else
+                if (RadioButton_Select.IsChecked == true)
+                    inkcanvas1.EditingMode = InkCanvasEditingMode.Select;
+                else
+                    if (RadioButton_EraseByStroke.IsChecked == true)
+                        inkcanvas1.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void slider1_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            inkcanvas1.DefaultDrawingAttributes.Height = (int)slider1.Value;
+            inkcanvas1.DefaultDrawingAttributes.Width = (int)slider1.Value;
+        }
+    }
+}
